c647be2977d56419eb248ea5e3f3836202f1d441
Oleg Nesterov <NZOlegok@gmail.com>
2018-05-15 19:07:59 +1200

    +form components

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -9,11 +9,56 @@ import classes from './ContactData.css'
 class ContactData extends Component {
 
 	state = {
-		name: '',
-		email: '',
-		address: {
-			street: '',
-			postalCode: ''
+		orderForm:{
+			name: {
+				elementType: 'input',
+				elementConfig:{
+					type: 'text',
+					placeholder: 'Your Name'
+				},
+				value: ''
+			},
+			street: {
+				elementType: 'input',
+				elementConfig:{
+					type: 'text',
+					placeholder: 'Street'
+				},
+				value: ''
+			},
+			zipCode:{
+				elementType: 'input',
+				elementConfig:{
+					type: 'text',
+					placeholder: 'ZIP Code'
+				},
+				value: ''
+			},
+			country:{
+				elementType: 'input',
+				elementConfig:{
+					type: 'text',
+					placeholder: 'Country'
+				},
+				value: ''
+			},
+			email: {
+				elementType: 'input',
+				elementConfig:{
+					type: 'email',
+					placeholder: 'Your E-Mail'
+				},
+				value: ''
+			},
+			deliveryMethod: {
+				elementType: 'select',
+				elementConfig:{
+					options:[{value: 'fastest', displayName: 'Fastest'},
+						{value: 'cheapest', displayName: 'Cheapest'},
+					]
+				},
+				value: ''
+			}
 		},
 		loading:false
 	};
@@ -24,16 +69,7 @@ class ContactData extends Component {
 		const order = {
 			ingredients: this.props.ingredients,
 			price: this.props.price,
-			customer: {
-				name: 'Oleg Nesterov',
-				address:{
-					street: '123 Road',
-					zipCode:'3020',
-					country:'NZ'
-				},
-				email: 'test@test.com'
-			},
-			deliveryMethod: 'fastest'
+
 		}
 		axios.post('/orders.json', order)
 			.then(response => {
@@ -45,13 +81,24 @@ class ContactData extends Component {
 	};
 
 	render() {
+		const formElementsArray = [];
+		for (let key in this.state.orderForm){
+			formElementsArray.push({
+				id: key,
+				config: this.state.orderForm[key]
+			});
+		}
 		let form = (				
 			<form>
-				<Input inputtype='input' type="text" name="name" placeholder="Your Name"/>
-				<Input inputtype='input' type="email" name="email" placeholder="Your Email"/>
-				<Input inputtype='input' type="text" name="street" placeholder="Your Street"/>
-				<Input inputtype='input' type="text" name="postal" placeholder="Your Postal Code"/>
-				<Button btnType="Success" clicked={this.orderHandler}> ORDER  </Button>
+				{formElementsArray.map(formElement => (
+					<Input
+						key={formElement.id}
+						elementType={formElement.config.elementType}
+								 elementConfig={formElement.config.elementConfig}
+					value={formElement.config.value}/>
+				))}
+				<Button btnType="Success" clicked={this.orderHandler}
+				> ORDER  </Button>
 		</form>);
 		if (this.state.loading){
 			form = <Spinner/>
